3916b3b56aa37454540abdb214f7ebac75aa68e2
Sobrino Julian <hello@julito.xyz>
2020-11-19 13:47:19 -0300

    Add a responsive navbar

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("./_site/tailwind.css");
+  eleventyConfig.addWatchTarget("_site/tailwind.css");
 
   eleventyConfig.addPassthroughCopy({
-    "./node_modules/alpinejs/dist/alpine.js": "./alpine.js",
+    "node_modules/alpinejs/dist/alpine.js": "alpine.js",
   });
 
-  eleventyConfig.addPassthroughCopy({ "./_static": "./static" });
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/@fortawesome/fontawesome-free/sprites": "static/sprites",
+  });
+
+  eleventyConfig.addPassthroughCopy({ "_static": "static" });
 
   return {
     dir: {
