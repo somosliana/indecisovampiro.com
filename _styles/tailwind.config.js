940d6c87d6ec1eba7acc73ba86542f05da51c926
Sobrino Julian <hello@julito.xyz>
2020-11-19 22:54:42 -0300

    *

--- _styles/tailwind.config.js
+++ _styles/tailwind.config.js
@@ -2,12 +2,21 @@ module.exports = {
   purge: ['./**/*.html'],
   darkMode: false,
   theme: {
+    aspectRatio: {
+      'none': 0,
+      'square': [1, 1],
+      '16/9': [16, 9],
+      '4/3': [4, 3],
+      '21/9': [21, 9],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [
-    require('tailwindcss-debug-screens')
+    require('tailwindcss-debug-screens'),
+    require("tailwindcss-responsive-embed"),
+    require("tailwindcss-aspect-ratio"),
   ]
 }
