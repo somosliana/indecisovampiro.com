3d90678c15308d8d1ae66efac9f2be895f02699a
Sobrino Julian <hello@julito.xyz>
2020-11-23 10:26:58 -0300

    *

--- _styles/tailwind.config.js
+++ _styles/tailwind.config.js
@@ -1,22 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  purge: ['./**/*.html'],
+  purge: ["./**/*.html"],
   darkMode: false,
   theme: {
     aspectRatio: {
-      'none': 0,
-      'square': [1, 1],
-      '16/9': [16, 9],
-      '4/3': [4, 3],
-      '21/9': [21, 9],
+      none: 0,
+      square: [1, 1],
+      "16/9": [16, 9],
+      "4/3": [4, 3],
+      "21/9": [21, 9],
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Barlow", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [
-    require('tailwindcss-debug-screens'),
+    require("tailwindcss-debug-screens"),
     require("tailwindcss-responsive-embed"),
     require("tailwindcss-aspect-ratio"),
-  ]
-}
+  ],
+};
