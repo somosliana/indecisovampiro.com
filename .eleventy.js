83a1e8457eefa749d7a76f9c2a0b38aeea6773a4
Sobrino Julian <sobrinojulian@pm.me>
2021-03-18 23:57:18 -0300

    Editorconfig && Prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,30 +1,30 @@
 module.exports = function (config) {
   // Watch!
-  config.addWatchTarget("_site/tailwind.css");
+  config.addWatchTarget('_site/tailwind.css')
 
   // Copy!
   // Favicon
-  config.addPassthroughCopy({ "static/favicon.ico": "favicon.ico" });
+  config.addPassthroughCopy({ 'static/favicon.ico': 'favicon.ico' })
   // Static
-  config.addPassthroughCopy("static");
+  config.addPassthroughCopy('static')
   // Netlify CMS
-  config.addPassthroughCopy("admin");
+  config.addPassthroughCopy('admin')
   // Vendor
   config.addPassthroughCopy({
-    "node_modules/alpinejs/dist/alpine.js": "alpine.js",
-    "node_modules/fitty/dist/fitty.min.js": "fitty.js",
-    "node_modules/@fortawesome/fontawesome-free/sprites": "static/sprites"
-  });
+    'node_modules/alpinejs/dist/alpine.js': 'alpine.js',
+    'node_modules/fitty/dist/fitty.min.js': 'fitty.js',
+    'node_modules/@fortawesome/fontawesome-free/sprites': 'static/sprites',
+  })
 
   // Config!
-  config.setBrowserSyncConfig(require('./lib/notFound'));
-  config.addTransform('minifyHtml', require('./lib/minifyHtml'));
+  config.setBrowserSyncConfig(require('./lib/notFound'))
+  config.addTransform('minifyHtml', require('./lib/minifyHtml'))
 
   return {
     dir: {
-      input: "_pages",
-      includes: "../_includes",
-      data: "../_data",
+      input: '_pages',
+      includes: '../_includes',
+      data: '../_data',
     },
-  };
-};
+  }
+}
